77eb9f350134c97029a048a8de3582c80f5bf7e7
Taylor Wayne <taywaydev@gmail.com>
2025-06-30 21:04:16 -0700

    friendship pair done and working

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import UserProfile
+from .models import FriendshipPair
 
 admin.site.register(UserProfile)
+admin.site.register(FriendshipPair)
--- backend/users/models.py
+++ backend/users/models.py
@@ -7,3 +7,18 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"Profile of {self.user.username}"
+    
+class FriendshipPair(models.Model):
+    user1 = models.ForeignKey(User, related_name='friend_1', on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, related_name='friend_2', on_delete=models.CASCADE)
+
+    status = models.CharField(max_length=20, default='pending')  # pending, accepted, rejected
+    is_confirmed = models.BooleanField(default=False)
+    bond_level = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user1', 'user2')
+
+    def __str__(self):
+        return f"{self.user1.username} and {self.user2.username})"
